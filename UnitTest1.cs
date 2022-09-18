5f6cea098c4827ff99006b1b85dbabebba52d8ec
omeryttnc <67800186+omeryttnc@users.noreply.github.com>
2022-09-18 22:28:41 +0100

    lesson 2

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,49 +1,49 @@
-//using Microsoft.Playwright;
+using Microsoft.Playwright;
 
-//namespace demoQA_playwright;
+namespace demoQA_playwright;
 
-//public class Tests
-//{
-//    [SetUp]
-//    public void Setup()
-//    {
-//    }
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
 
-//    [Test]
-//    public async Task Test1()
-//    {
-//        //Playwright
-//        using var playwright = await Playwright.CreateAsync();
-//        //Browser
-//        await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
-//        {
-//            Headless =false
-//        });
-//        //Page
-//        var page = await browser.NewPageAsync();
+    [Test]
+    public async Task Test1()
+    {
+        //Playwright
+        using var playwright = await Playwright.CreateAsync();
+        //Browser
+        await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+        {
+            Headless = false
+        });
+        //Page
+        var page = await browser.NewPageAsync();
 
-//        await page.GotoAsync("https://demoqa.com/");
-//        await page.ClickAsync("#app > div > div > div.home-body > div > div:nth-child(1) > div > div.avatar.mx-auto.white > svg");
-     
-
-//        await page.ClickAsync("(//span[@class='text'])[1]");
-     
+        await page.GotoAsync("https://demoqa.com/");
+        await page.ClickAsync("#app > div > div > div.home-body > div > div:nth-child(1) > div > div.avatar.mx-auto.white > svg");
 
 
-//        await page.FillAsync("#userName", "ben username");
-//        await page.FillAsync("#userEmail", "oasd@gmail.com");
-//        await page.FillAsync("#currentAddress", "ben currentAddress");
-
-//        await page.ClickAsync("#submit");
-
-//        await page.ScreenshotAsync(new PageScreenshotOptions
-//        {
-//            Path = "EAApp.jpg"
-//        });
-
-//        var isExist = await page.Locator("#name").IsVisibleAsync();
-//        Assert.IsTrue(isExist);
+        await page.ClickAsync("(//span[@class='text'])[1]");
 
 
-//    }
-//}
+
+        await page.FillAsync("#userName", "ben username");
+        await page.FillAsync("#userEmail", "oasd@gmail.com");
+        await page.FillAsync("#currentAddress", "ben currentAddress");
+
+        await page.ClickAsync("#submit");
+
+        await page.ScreenshotAsync(new PageScreenshotOptions
+        {
+            Path = "EAApp.jpg"
+        });
+
+        var isExist = await page.Locator("#name").IsVisibleAsync();
+        Assert.IsTrue(isExist);
+
+
+    }
+}
